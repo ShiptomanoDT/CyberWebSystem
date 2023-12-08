44b05e26946e7462aedc98730e10aa20df25a185
Frankenio <emacu300@gmail.com>
2023-12-08 19:09:56 -0400

    Agregando el LOGIN en el proyecto
    Se conecto Login con la pagina

--- CyberWebSystem/Controllers/LoginController.cs
+++ CyberWebSystem/Controllers/LoginController.cs
@@ -1,17 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CyberWebSystem.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
 using System.Runtime.CompilerServices;
 
 namespace CyberWebSystem.Controllers
 {
     public class LoginController : Controller
     {
+        private MiContext _context;
+        public LoginController(MiContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
         public async Task<IActionResult> Login(string email, string password)
         {
-            return RedirectToAction("Index");
+            var usuario = await _context.Usuarios
+                                        .Where(x => x.Email == email && x.Password == password)
+                                        .FirstOrDefaultAsync();
+            if (usuario != null)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                TempData["LoginError"] = "Cuenta o Password Incorrecto!";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
