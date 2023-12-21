61f9f0c96e57c5458f2b09d24670a8f6d0c531cb
Waldo <shiptomano@gmail.com>
2023-12-21 19:56:04 -0400

    Se logro que se muestre el usuario correspondiente a la maquina en Details de Cliente, se agregaron iconos en algunos interfaces en la aprte de los titulos

--- CyberWebSystem/Controllers/ClientesController.cs
+++ CyberWebSystem/Controllers/ClientesController.cs
@@ -10,44 +10,48 @@ using CyberWebSystem.Models;
 
 namespace CyberWebSystem.Controllers
 {
-    public class ClientesController : Controller
-    {
-        private readonly MiContext _context;
+	public class ClientesController : Controller
+	{
+		private readonly MiContext _context;
 
-        public ClientesController(MiContext context)
-        {
-            _context = context;
-        }
+		public ClientesController(MiContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Clientes
-        public async Task<IActionResult> Index()
-        {
-              return _context.Clientes != null ? 
-                          View(await _context.Clientes.ToListAsync()) :
-                          Problem("Entity set 'MiContext.Clientes'  is null.");
-        }
+		// GET: Clientes
+		public async Task<IActionResult> Index()
+		{
+			return _context.Clientes != null ?
+						View(await _context.Clientes.ToListAsync()) :
+						Problem("Entity set 'MiContext.Clientes'  is null.");
+		}
 
-        // GET: Clientes/Details/
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Clientes == null)
-            {
-                return NotFound();
-            }
+		// GET: Clientes/Details/
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.Clientes == null)
+			{
+				return NotFound();
+			}
 
-            var cliente = await _context.Clientes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-            //Cargando datos de equipos sin alterar el contexto
-            var equipos = await _context.Equipos.ToListAsync();
-            //Agregando los Equipos a Viewdata
-            ViewData["Equipos"] = equipos;
+			var cliente = await _context.Clientes
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (cliente == null)
+			{
+				return NotFound();
+			}
+			//Cargando datos de equipos sin alterar el contexto
+			var equipos = await _context.Equipos.ToListAsync();
+			//Agregando los Equipos a Viewdata
+			ViewData["Equipos"] = equipos;
 
-            return View(cliente);
-        }
+			var fletes = await _context.Flete.Include(f => f.Cliente)
+										.ToListAsync();
+			ViewData["Fletes"] = fletes;
+
+			return View(cliente);
+		}
 
 		// GET: Clientes/VerFlete/
 		public IActionResult ClienteFletes(int id)
@@ -66,117 +70,117 @@ namespace CyberWebSystem.Controllers
 
 		// GET: Clientes/Create
 		public IActionResult Create()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
 
-        // POST: Clientes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,NombreCompleto,Ci")] Cliente cliente)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(cliente);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cliente);
-        }
+		// POST: Clientes/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,NombreCompleto,Ci")] Cliente cliente)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(cliente);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(cliente);
+		}
 
-        // GET: Clientes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Clientes == null)
-            {
-                return NotFound();
-            }
+		// GET: Clientes/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || _context.Clientes == null)
+			{
+				return NotFound();
+			}
 
-            var cliente = await _context.Clientes.FindAsync(id);
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-            return View(cliente);
-        }
+			var cliente = await _context.Clientes.FindAsync(id);
+			if (cliente == null)
+			{
+				return NotFound();
+			}
+			return View(cliente);
+		}
 
-        // POST: Clientes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,NombreCompleto,Ci")] Cliente cliente)
-        {
-            if (id != cliente.Id)
-            {
-                return NotFound();
-            }
+		// POST: Clientes/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,NombreCompleto,Ci")] Cliente cliente)
+		{
+			if (id != cliente.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(cliente);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ClienteExists(cliente.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cliente);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(cliente);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ClienteExists(cliente.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(cliente);
+		}
 
-        // GET: Clientes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Clientes == null)
-            {
-                return NotFound();
-            }
+		// GET: Clientes/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null || _context.Clientes == null)
+			{
+				return NotFound();
+			}
 
-            var cliente = await _context.Clientes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (cliente == null)
-            {
-                return NotFound();
-            }
+			var cliente = await _context.Clientes
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (cliente == null)
+			{
+				return NotFound();
+			}
 
-            return View(cliente);
-        }
+			return View(cliente);
+		}
 
-        // POST: Clientes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Clientes == null)
-            {
-                return Problem("Entity set 'MiContext.Clientes'  is null.");
-            }
-            var cliente = await _context.Clientes.FindAsync(id);
-            if (cliente != null)
-            {
-                _context.Clientes.Remove(cliente);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: Clientes/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (_context.Clientes == null)
+			{
+				return Problem("Entity set 'MiContext.Clientes'  is null.");
+			}
+			var cliente = await _context.Clientes.FindAsync(id);
+			if (cliente != null)
+			{
+				_context.Clientes.Remove(cliente);
+			}
 
-        private bool ClienteExists(int id)
-        {
-          return (_context.Clientes?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-    }
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool ClienteExists(int id)
+		{
+			return (_context.Clientes?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
 }
